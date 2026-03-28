4adf610b9468a0ebad9559833518e8dbd4e29675
unknown <COMPLAB4@COMPLAB4-14.ASIANCOLLEGE.LOCAL>
2026-03-28 17:48:08 +0800

    dahjahg

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />   {/* VERY IMPORTANT */}
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
